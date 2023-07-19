caf326ffc8f918b443036702f1568fb1755bbddc
Sharleen Beniamin <sharleenb16@gmail.com>
2023-07-19 00:11:27 +0000

    user spec and testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,30 @@ class User < ApplicationRecord
 
   has_secure_password
   
+  validates :first, presence: true
+  validates :last, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  # validates :password_confirmation, presence: true
+
+  before_create :format_email
+
+  def self.authenticate_with_credentials(email, password)
+    @email = email.strip
+    @email_lowercase = @email.downcase
+    @user = User.find_by_email(@email_lowercase)
+
+    if @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
+
+  private
+
+  def format_email
+    self.email = email.strip
+    self.email = self.email.downcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'Validations' do
+    it 'should show error is password and password confirmation do not match' do
+      @user = User.new
+      @user.password = 'test123'
+      @user.password_confirmation = 'test321'
+      @user.save
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'should show error is email is not unique' do
+      @user1 = User.new(first: 'Sharleen', last: 'Beniamin', email: 'sharleenb16@gmail.com', password: 'test321', password_confirmation: 'test321')
+      @user1.save
+      @user2 = User.new(first: 'Shar', last: 'Beniamin', email: 'sharleenb16@gmail.com', password: 'test321', password_confirmation: 'test321')
+      @user2.save
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it 'should show error if email not entered' do
+      @user = User.new(first: 'Sharleen', last: 'Beniamin', email: '', password: 'test321', password_confirmation: 'test321')
+      @user.save
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it 'should show error if first name not entered' do
+      @user = User.new(first: '', last: 'Beniamin', email: 'sharleenb16@gmail.com', password: 'test321', password_confirmation: 'test321')
+      @user.save
+      expect(@user.errors.full_messages).to include("First can't be blank")
+    end
+
+    it 'should show error if last name not entered' do
+      @user = User.new(first: 'Sharleen', last: '', email: 'sharleenb16@gmail.com', password: 'test321', password_confirmation: 'test321')
+      @user.save
+      expect(@user.errors.full_messages).to include("Last can't be blank")
+    end
+
+    it'should show error if password is not more than 5 characters' do
+      @user = User.new(first: 'Sharleen', last: 'Beniamin', email: 'sharleenb16@gmail.com', password: 'test3', password_confirmation: 'test3')
+      @user.save
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+
+  end
+
+  describe '.authenticate_with_credentials' do
+    it 'should only log in successfully if credentials are valid' do
+      @user = User.new(first: 'Shar', last: 'Beniamin', email: 'sharleenb16@gmail.com', password: 'test321', password_confirmation: 'test321')
+      @user.save
+      @user_logged_in = User.authenticate_with_credentials('sharleenb16@gmail.com', 'test321')
+      expect(@user_logged_in).to_not eq(nil)
+    end
+
+    it 'should log in successfully even if there are trailing spaces in email address' do
+      @user = User.new(first: 'Shar', last: 'Beniamin', email: ' sharleenb16@gmail.com', password: 'test321', password_confirmation: 'test321')
+      @user.save
+      @user_logged_in = User.authenticate_with_credentials(' sharleenb16@gmail.com', 'test321')
+      expect(@user_logged_in).to_not eq(nil)
+    end
+
+    it 'should log in successfully regardless of case in email address' do
+      @user = User.new(first: 'Shar', last: 'Beniamin', email: 'SHARleenb16@gmail.com', password: 'test321', password_confirmation: 'test321')
+      @user.save
+      @user_logged_in = User.authenticate_with_credentials('SHARleenb16@gmail.com', 'test321')
+      expect(@user_logged_in).to_not eq(nil)
+    end
+
+  end
+  
 end
